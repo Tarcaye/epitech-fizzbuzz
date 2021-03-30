fc026bac43bb52ba07b149b073af8dd92bc1800a
Tarcaye <thomas.carpaye@gmail.com>
2021-03-30 15:48:06 +0200

    Third round

--- src/main/es/main.js
+++ src/main/es/main.js
@@ -33,7 +33,13 @@ const simpleDigits = [
 
 export const lcdConverter = {
   toLCD(number) {
-    return simpleDigits[number];
+    let word = number.toString();
+    let res = "";
+
+    for (let index = 0; index < word.length; index++) {
+      res += simpleDigits[word[index]];
+    }
+    return res;
     // Conseil de l'equipe 6: pour multipleDigits -> %10 puis /10 pour afficher les chiffres précédents. Boucler tant que Nb =/= 0.
   },
   fromLCD(lcdStringRepresentation) {
